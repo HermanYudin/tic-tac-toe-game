3001e86e8897fbaea93f6107d1cf99eed8975f5b
HermanYudin <hermanhockey1994@yandex.ru>
2023-10-10 09:51:59 +0300

    feat:game done

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const blackout = document.querySelector('.blackout');
 const modal = document.querySelector('.modal');
 const modalTitle = document.querySelector('.modal-title');
 const modalBtn = document.querySelector('.modal-button');
+let timer = document.querySelector('.timer');
+let time = 0;
+let timerTwo = document.querySelector('.timer-two');
 
 function changePlayer(event){
 if(event.target.className = 'box'){
@@ -47,16 +50,22 @@ function winnerCheck(){
 
     for(let i = 0; i < arr.length; i++){
         if(boxes[arr[i][0]].innerHTML === 'X' && boxes[arr[i][1]].innerHTML === 'X'&& boxes[arr[i][2]].innerHTML === 'X'){
+            clearInterval(timerId);
+            timerTwo.innerHTML = timer.innerHTML;
             blackout.classList.add('block');
             modal.classList.add('flex');
             modalTitle.innerHTML = 'Tics won!';
         }
         else if(boxes[arr[i][0]].innerHTML === 'O' && boxes[arr[i][1]].innerHTML === 'O'&& boxes[arr[i][2]].innerHTML === 'O'){
+            clearInterval(timerId);
+            timerTwo.innerHTML = timer.innerHTML;
             blackout.classList.add('block');
             modal.classList.add('flex');
             modalTitle.innerHTML = 'Tacs won!';
         }
         else if(Array.from(boxes).every((el) => el.innerHTML !== '')){
+            clearInterval(timerId);
+            timerTwo.innerHTML = timer.innerHTML;
             blackout.classList.add('block');
             modal.classList.add('flex');
             modalTitle.innerHTML = 'Nobody won!';
@@ -66,6 +75,25 @@ function winnerCheck(){
 
 document.body.addEventListener("click", function (event) {
     if(event.target === blackout || event.target === modalBtn){
+        localStorage.setItem("Score",timer.innerHTML);
         location.reload();
     }
 });
+
+function timerCount (){
+    let minutes = Math.floor(time/60);
+    let seconds = time % 60;
+    if(seconds < 10 && minutes < 10){
+        seconds = '0' + seconds;
+    }
+        else{
+            seconds;
+        }
+        timer.innerHTML = minutes + ':' + seconds;
+        time++;
+    }
+
+    let timerId;
+    gameContainer.addEventListener('click', () => {
+      timerId = setInterval(() => timerCount(), 1000);
+      }, { once: true });
